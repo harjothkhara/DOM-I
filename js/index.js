3e60a4249a0c86ed5321c3dd5905600bfa9697ee
harjothkhara <harjoth.khara@gmail.com>
2019-04-08 12:01:04 -0700

    added img selectors

--- js/index.js
+++ js/index.js
@@ -38,5 +38,12 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let logoImg = document.getElementById("logo-img");
+logoImg.setAttribute("src", "img/logo.png");
+
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute("src", "img/header-img.png")
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute("src", "img/mid-page-accent.jpg")
